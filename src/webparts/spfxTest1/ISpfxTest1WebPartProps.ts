1c3c4b53697bb17715f605b5b583537ea8c949fd
chara75 <susanjchin@gmail.com>
2017-06-09 17:59:22 -0600

    initial version

---
+++ src/webparts/spfxTest1/ISpfxTest1WebPartProps.ts
@@ -0,0 +1,3 @@
+export interface ISpfxTest1WebPartProps {
+  description: string;
+}
---
+++ src/webparts/spfxTest1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxTest1Strings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'spfxTest1Strings' {
+  const strings: ISpfxTest1Strings;
+  export = strings;
+}
